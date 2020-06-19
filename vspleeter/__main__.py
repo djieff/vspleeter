b3a984961bdeafd1d184bc09da9cbf639f8e0e83
Jean-Francois Bouchard <bouchard.jfrancois@gmail.com>
2020-06-19 17:58:41 -0400

    cleanup

--- vspleeter/__main__.py
+++ vspleeter/__main__.py
@@ -1,4 +1,4 @@
-"""
+"""UI Wrapper script over the "spleeter separate" command
 """
 import os
 import sys
@@ -6,13 +6,19 @@ import subprocess
 
 import itertools
 
+from PySide2 import QtCore
 from PySide2.QtWidgets import QApplication
 from PySide2.QtUiTools import QUiLoader
 
 BASE_COMMAND = "{binaryName} separate -i {inputFilePath} -p spleeter:{stemNum}stems -o {outputDir}"
 OUTPUT_PATH_SUFFIX = "{rootOutputDir}/{basename}_spleeted/{binaryType}/{stemNum}stems/"
 
+
 def main():
+    """main function
+    """
+    QtCore.QCoreApplication.setAttribute(QtCore.Qt.AA_ShareOpenGLContexts)
+
     app = QApplication(sys.argv)
     app.setOrganizationName("djieffx")
     app.setApplicationName("vspleeter")
@@ -20,12 +26,22 @@ def main():
     mw = loader.load(os.path.join(os.path.dirname(__file__), "vspleeter_mainWindow.ui"))
 
     def getBinaries():
+        """Create a generator that yield the proper command name if the checkbox is checked.
+
+        :return: generator with command name
+        :rtype: generator
+        """
         if mw.cpuCheckBox.checkState():
             yield 'spleeter'
         if mw.gpuCheckBox.checkState():
             yield 'spleeter-gpu'
 
     def getStemsAmount():
+        """Create a generator that yield the stems amount if the checkbox is checked.
+
+        :return: generator with stem count
+        :rtype: generator
+        """
         if mw.stems2CheckBox.checkState():
             yield '2'
         if mw.stems4CheckBox.checkState():
@@ -33,9 +49,16 @@ def main():
         if mw.stems5CheckBox.checkState():
             yield '5'
 
-    def createOutputDir(outputSpecs):
+    def createOutputDir(cmdSpecs):
+        """Create the output directory from defined output specifications
 
-        outputDir = OUTPUT_PATH_SUFFIX.format(**outputSpecs)
+        :param cmdSpecs: context based variables to execute spleeter command
+        :type cmdSpecs: dict
+
+        :return: the output directory
+        :rtype: str
+        """
+        outputDir = OUTPUT_PATH_SUFFIX.format(**cmdSpecs)
         if not os.path.exists(outputDir):
             os.makedirs(outputDir, mode=0o777)
             print('Created the {0} directory'.format(outputDir))
@@ -44,46 +67,62 @@ def main():
 
     def generateCommandsPerElements(binaryGenerator, inputFilePath, stemsCPUGenerator, stemsGPUGenerator,
                                     rootOutputDir):
-        basename = os.path.basename(os.path.splitext(inputFilePath)[0])
-        binaryType = 'CPU'
-        stemsGenerator = stemsCPUGenerator
-        for binaryName in binaryGenerator:
-            if 'gpu' in binaryName:
-                binaryType = 'GPU'
-                stemsGenerator = stemsGPUGenerator
-                yield from generateCmdPerStem(binaryName, binaryType, basename, inputFilePath, rootOutputDir,
-                                              stemsGenerator)
-            else:
-                yield from generateCmdPerStem(binaryName, binaryType, basename, inputFilePath, rootOutputDir,
-                                              stemsGenerator)
+        """Generate all the commands necessary, per what is selected in the UI, looping on the process type
 
-    def generateCmdPerStem(binaryName, binaryType, basename, inputFilePath, rootOutputDir, stemsGenerator):
+        :param binaryGenerator: generator with all the binary selections
+        :param str inputFilePath: The file to be spleeted
+        :param stemsCPUGenerator: generator with the CPU processes flag
+        :param stemsGPUGenerator: generator with the GPU processes flag
+        :param str rootOutputDir: The output directory
+
+        :return: generator with all command line to spleet the file
+        """
+        for binaryName in binaryGenerator:
+            cmdSpecs = {
+                'rootOutputDir': rootOutputDir,
+                'basename': os.path.basename(os.path.splitext(inputFilePath)[0]),
+                'binaryType': 'CPU',
+                'binaryName': binaryName,
+                'inputFilePath': inputFilePath,
+                'stemsGenerator': stemsCPUGenerator
+            }
+            if 'gpu' in binaryName:
+                cmdSpecs['binaryType'] = 'GPU'
+                cmdSpecs['stemsGenerator'] = stemsGPUGenerator
+                yield from generateCmdPerStem(cmdSpecs)
+            else:
+                yield from generateCmdPerStem(cmdSpecs)
+
+    def generateCmdPerStem(cmdSpecs):
+        """Generate the command from the stem count
+
+        :param dict cmdSpecs: a dictionary containing all necessary data to generate the command
+
+        :return: a command to execute
+        :rtype: list
+        """
+        stemsGenerator = cmdSpecs['stemsGenerator']
         for stemNum in stemsGenerator:
-            outputSpecs = {'rootOutputDir': rootOutputDir,
-                           'basename': basename,
-                           'binaryType': binaryType,
-                           'stemNum': stemNum
-                           }
-            outputDir = createOutputDir(outputSpecs)
-            cmdSpecs = {'binaryName': binaryName,
-                        'inputFilePath': inputFilePath,
-                        'stemNum': stemNum,
-                        'outputDir': outputDir
-                        }
+            cmdSpecs['stemNum'] = stemNum
+            cmdSpecs['outputDir'] = createOutputDir(cmdSpecs)
             cmd = BASE_COMMAND.format(**cmdSpecs)
             cmd = cmd.split(' ')
             yield cmd
 
-    def processBatchElements(index):
+    def processBatchElements(_):
+        """Process all the data from the UI, and execute all the commands generated
 
+        :param _: unused input
+        """
         binaryGenerator = getBinaries()
         inputFilePath = mw.inputFileLineEdit.text()
         stemsCPUGenerator = getStemsAmount()
         stemsCPUGenerator, stemsGPUGenerator = itertools.tee(stemsCPUGenerator)
         rootOutputDir = mw.outputDirLineEdit.text()
 
-        generatedCmds = generateCommandsPerElements(binaryGenerator, inputFilePath, stemsCPUGenerator,
-                                                    stemsGPUGenerator, rootOutputDir)
+        generatedCmds = generateCommandsPerElements(
+            binaryGenerator, inputFilePath, stemsCPUGenerator, stemsGPUGenerator, rootOutputDir
+        )
 
         for cmd in generatedCmds:
             prettyCmd = ' '.join(cmd)
@@ -91,7 +130,7 @@ def main():
             subprocess.run(cmd)
             print("Command Success\n\n")
 
-
+        print("All Operations Successful")
 
     mw.processPushButton.clicked.connect(processBatchElements)
     mw.show()
